9862c5aded335d75939f437043b29bbd70131029
Steen Hegelund <steen@hegelund-hansen.dk>
2022-04-10 21:44:40 +0200

    open diff session in commit session on diff lines or fold section headers

--- lua/commit_session.lua
+++ lua/commit_session.lua
@@ -1,11 +1,30 @@
 -- Steen Hegelund
--- Time-Stamp: 2022-Apr-10 21:14
+-- Time-Stamp: 2022-Apr-10 21:43
 -- Provide a Git commit session
 -- vim: set ts=2 sw=2 sts=2 tw=120 et cc=120 ft=lua :
 local Module = {}
 
 local gs = require('git_session')
+local gfds = require('file_diff_session')
+
 local GitCommitSession = gs.GitSession:new()
+
+Module.open = function()
+  local ses = gs.find(vim.api.nvim_get_current_buf())
+  local commitpath = string.match(vim.api.nvim_get_current_line(), ' (%S+)%s+%| [+-]*')
+  if commitpath then
+    return gfds.new(ses.cwd, ses.commit, commitpath, ses.commit .. '~1')
+  end
+  commitpath = string.match(vim.api.nvim_get_current_line(), 'diff %S+ a%/(%S+)%s+b%/%S+')
+  if commitpath then
+    return gfds.new(ses.cwd, ses.commit, commitpath, ses.commit .. '~1')
+  end
+  commitpath = string.match(vim.api.nvim_get_current_line(), '[+-]+ %d+ lines: diff %S+ a/(%S+)%s+b/%S+')
+  if commitpath then
+    return gfds.new(ses.cwd, ses.commit, commitpath, ses.commit .. '~1')
+  end
+  print('not found:', vim.api.nvim_get_current_line())
+end
 
 Module.close = function()
   gs.find(vim.api.nvim_get_current_buf()):quit()
@@ -37,6 +56,7 @@ Module.new = function(cwd, commit)
     commit = commit,
     filetype = 'diff',
     keymap = {
+      go = gs.key_handler('commit_session_open', Module.open),
       gq = gs.key_handler('commit_session_close', Module.close),
     },
     callback = function()
